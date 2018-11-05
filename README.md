f129af5921bf6dd483fda1eb2f9bdde091281248
Radovan Balog <radovan.balog@2ring.com>
2018-11-05 16:59:58 +0100

    Version 0.0.2

--- README.md
+++ README.md
@@ -5,28 +5,50 @@ The 2Ring Markdown Extension should be used for editing internal documentation f
 ## Features
 
 The extension contains support for the following markup:
+> ```
+> ::: classNameOne classNameTwo
+> content
+> :::
+> ```
+> results in:
+> ```html
+> <div class="classNameOne classNameTwo">content<div>
+> ```
 
-```
-Containers
-::: attention
-:::
+> ```plain
+> :fa-icon:
+> :fas-icon:
+> :far-icon:
+> :fab-icon:
+> ```
+> results in:
+> ```html
+> <i class="fa fa-icon"></i>
+> <i class="fas fa-icon"></i>
+> <i class="far fa-icon"></i>
+> <i class="fab fa-icon"></i>
+> ```
 
-::: notice
-:::
+> ```plain
+> [ ] for checkboxes
+> [x] for filled checkboxes
+> ```
 
-Font Awesome icons
-:fa-icon:
-:fas-icon:
-:far-icon:
-:fab-icon:
-
-[ ] for checkboxes
-[x] for filled checkboxes
-
-^button^ for buttons
-```
+> ```plain
+> ^button^
+> ^button:btn btn-primary^
+> ```
+> results in:
+> ```html
+> <button>button</button>
+> <button class="btn btn-primary">button</button>
+> ```
 
 ## Release Notes
+
+### 0.0.2
+- Support for any container
+- Support for button classes
 
 ### 0.0.1
 
--- src/extendMarkdown.ts
+++ src/extendMarkdown.ts
@@ -1,10 +1,26 @@
 export class MarkdownExtender {
     public static extendMarkdownIt = (md: any): any => {
+        const containerOptions = {
+            validate: (params: string) => {
+                return params.trim().match(/\w+/);
+            },
+            render: (tokens: Array<any>, idx: number) => {
+                const classes = tokens[idx].info.trim().match(/^(\w+ ?)+$/);
+
+                if (tokens[idx].nesting === 1) {
+                    // opening tag
+                    return `<div class="${md.utils.escapeHtml(classes[0])}">`;
+                } else {
+                    // closing tag
+                    return '</div>\n';
+                }
+            }
+        };
+
         return md.use(require('markdown-it-anchor-2ring'), {
             level: [1, 2, 3]
         }).use(require('markdown-it-highlightjs'))
-        .use(require('markdown-it-container'), 'notice')
-        .use(require('markdown-it-container'), 'attention')
+        .use(require('markdown-it-container'), 'container', containerOptions)
         .use(require('markdown-it-fontawesome'))
         .use(require('markdown-it-checkbox'))
         .use(require('markdown-it-button'));
