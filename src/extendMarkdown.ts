3c3cf41ee8569384a4cd27d212b30d5ef0adc9d1
Radovan Balog <radovan.balog@2ring.com>
2019-01-08 11:07:13 +0100

    Variable toc level

--- src/extendMarkdown.ts
+++ src/extendMarkdown.ts
@@ -1,8 +1,10 @@
+import { workspace } from 'vscode';
+
 export class MarkdownExtender {
     public static extendMarkdownIt = (md: any): any => {
         const containerOptions = {
             validate: (params: string) => {
-                return params.trim().match(/\w+/);
+                return params.trim().match(/^(\w+ ?)+$/);
             },
             render: (tokens: Array<any>, idx: number) => {
                 const classes = tokens[idx].info.trim().match(/^(\w+ ?)+$/);
@@ -18,7 +20,8 @@ export class MarkdownExtender {
         };
 
         return md.use(require('markdown-it-anchor-2ring'), {
-            level: [1, 2, 3]
+            level: [1, 2, 3],
+            tocLevel: workspace.getConfiguration('2r_md').tocLevel
         }).use(require('markdown-it-highlightjs'))
         .use(require('markdown-it-container'), 'container', containerOptions)
         .use(require('markdown-it-fontawesome'))
